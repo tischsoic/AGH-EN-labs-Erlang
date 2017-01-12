29d52542516462ed9d17220558877b80cef99ca9
Jakub Brzegowski <symfiz@gmail.com>
2017-01-12 18:22:32 +0100

    Added types for functions in module soe_p.

--- sieve_of_eratosthenes/soe_p.erl
+++ sieve_of_eratosthenes/soe_p.erl
@@ -1,17 +1,23 @@
 -module('soe_p').
 -export([get_primes/2, sieve_thread_main/3, sieve_thread/2, set_numbers/1]).
 
+-type list_of_numbers() :: list(integer()).
+
+-spec get_primes(Max_numb::integer(), Threads_no::integer()) -> list_of_numbers().
 get_primes(2, _) -> [2];
 get_primes(3, _) -> [2, 3];
 get_primes(Max_numb, Threads_no) ->
     %c:flush(),
     Numbers = set_numbers(Max_numb),
     Primes = sieve(Max_numb, Numbers, Threads_no),
-    Primes_with_2 = [2, 3 | Primes].
+    Primes_with_2 = [2, 3 | Primes],
+    Primes_with_2.
 
+-spec set_numbers(Max_numb::Integer) -> list(Integer).
 set_numbers(Max_numb) ->
     [5 | set_numbers_2(Max_numb, 5)].
 
+-spec set_numbers_2(Max_numb::Integer, Base::Integer) -> list(Integer).
 set_numbers_2(Max_numb, Base) ->
     Next_numb = Base + 2,
     if
@@ -21,6 +27,7 @@ set_numbers_2(Max_numb, Base) ->
             [Next_numb | set_numbers_4(Max_numb, Next_numb)]
     end.
 
+-spec set_numbers_4(Max_numb::Integer, Base::Integer) -> list(Integer).
 set_numbers_4(Max_numb, Base) ->
     Next_numb = Base + 4,
     if
@@ -30,13 +37,16 @@ set_numbers_4(Max_numb, Base) ->
             [Next_numb | set_numbers_2(Max_numb, Next_numb)]
     end.
 
+-spec sieve_thread_main(Numbers::list_of_numbers(), Max_numb::integer(), Parent_pid::pid()) -> true.
 sieve_thread_main(Numbers, Max_numb, Parent_pid) ->
     %io:fwrite("tutaj 2"),
     Primes = sieve_thread_main_(Numbers, Max_numb, Parent_pid),
     %io:fwrite("tutaj 1"),
     Parent_pid ! {end_of_main},
-    Parent_pid ! {primes, self(), Primes}.
+    Parent_pid ! {primes, self(), Primes},
+    true.
 
+-spec sieve_thread_main_(list_of_numbers(), Max_numb::integer(), Parent_pid::pid()) -> list_of_numbers().
 sieve_thread_main_([], _, _) -> [];
 sieve_thread_main_([Prime | T], Max_numb, Parent_pid) ->
     if
@@ -50,10 +60,13 @@ sieve_thread_main_([Prime | T], Max_numb, Parent_pid) ->
             [Prime | sieve_thread_main_(Sieved_list, Max_numb, Parent_pid)]
     end.
 
+-spec sieve_thread(Numbers::list_of_numbers(), Parent_pid::pid()) -> true.
 sieve_thread(Numbers, Parent_pid) ->
     Sieved_list = sieve_thread_(Numbers),
-    Parent_pid ! {primes, self(), Sieved_list}.
+    Parent_pid ! {primes, self(), Sieved_list},
+    true.
 
+-spec sieve_thread_(Numbers::list_of_numbers()) -> list_of_numbers().
 sieve_thread_(Numbers) ->
     receive
         {end_of_sieving} ->
@@ -67,26 +80,32 @@ sieve_thread_(Numbers) ->
             sieve_thread_(Sieved_list)
     end.
 
+-spec split_into(List::list_of_numbers(), Sublists_no::integer()) -> list(list_of_numbers()).
 split_into(List, Sublists_no) ->
     Sublist_length = length(List),
     %io:fwrite(io_lib:format("~p", [Sublist_length])),
     %io:fwrite(io_lib:format("~p", [Sublists_no])),
     Chunks_length = Sublist_length div Sublists_no,
-    Chunks = split_into_(List, Chunks_length + 1, Sublists_no).
+    Chunks = split_into_(List, Chunks_length + 1, Sublists_no),
+    Chunks.
 
+-spec split_into_(List::list_of_numbers(), Chunks_length::integer(), Sublists_no::integer()) -> list(list_of_numbers()).
 split_into_(List, _, 1) -> [List];
 split_into_(List, Chunks_length, Sublists_no) ->
     {Chunk, Rest} = lists:split(Chunks_length, List),
     [Chunk | split_into_(Rest, Chunks_length, Sublists_no - 1)].
 
+-spec start_threads(list(list_of_numbers()), Max_numb::integer(), Threads_no::integer()) -> list(pid()).
 start_threads([Main_chunk | Rest_chunks], Max_numb, Threads_no) ->
     Parent_pid = self(),
     Main_pid = start_thread_main(Main_chunk, Max_numb, Parent_pid),
     Rest_pids = start_threads_rest(Rest_chunks, Parent_pid),
     [Main_pid | Rest_pids].
 
+-spec start_thread_main(list_of_numbers(), Max_numb::integer(), Parent_pid::pid()) -> pid().
 start_thread_main(Main_chunk, Max_numb, Parent_pid) ->
-    Pid = spawn(?MODULE, sieve_thread_main, [Main_chunk, Max_numb, Parent_pid]).
+    Pid = spawn(?MODULE, sieve_thread_main, [Main_chunk, Max_numb, Parent_pid]),
+    Pid.
 
 start_threads_rest([], _) -> [];
 start_threads_rest([Chunk | Rest_chunks], Parent_pid) ->
@@ -138,7 +157,8 @@ sieve(Max_numb, Numbers, Threads_no) ->
     Numbers_chunks = split_into(Numbers, Threads_no_final),
     [Main_pid | Rest_pids] = start_threads(Numbers_chunks, Max_numb, Threads_no_final),
     threads_communication([Main_pid | Rest_pids], Max_numb),
-    Primes = gather_all_primes([Main_pid | Rest_pids]).
+    Primes = gather_all_primes([Main_pid | Rest_pids]),
+    Primes.
     %case lists:flatten(Numbers_rest) of
         %[] -> Primes;
         %Numbers_to_sieve ->
